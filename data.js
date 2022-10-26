47960e488b460605bd59fe699b8bf5d268ddfaab
Heberlis <heberlisfinol@gmail.com>
2022-10-26 19:50:24 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,10 @@
+// estas funciones son de ejemplo
+
+export const mostrarLibros = (data) => {
+const book= data.
+  return 'books';
+};
+
+export const anotherExample = () => {
+  return 'OMG';
+};
---
+++ main.js
@@ -0,0 +1,13 @@
+import data from './data/harrypotter/data.js';
+// Mostar saga de los libros
+const libros = data.books
+
+libros.forEach(title =>(title.title));
+
+function mostrar (){   
+    const libros= data.title;
+    return   libros}
+//import data from './data/harrypotter/data.js';
+
+
+//console.log( data.title);
